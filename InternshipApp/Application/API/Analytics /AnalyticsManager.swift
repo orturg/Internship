66757e2e2d394de1cd0bf35c881c0ccb47befa67
Artur <a.nozhenko@ukma.edu.ua>
2024-10-01 08:26:33 +0300

    Added deeplinks

---
+++ InternshipApp/Application/API/Analytics /AnalyticsManager.swift
@@ -0,0 +1,8 @@
+//
+//  AnalyticsManager.swift
+//  InternshipApp
+//
+//  Created by Artur Nozhenko on 29.09.2024.
+//
+
+import Foundation
---
+++ InternshipApp/Application/Deeplinks/DeepLink.swift
@@ -0,0 +1,8 @@
+//
+//  DeepLink.swift
+//  InternshipApp
+//
+//  Created by Artur Nozhenko on 30.09.2024.
+//
+
+import Foundation
